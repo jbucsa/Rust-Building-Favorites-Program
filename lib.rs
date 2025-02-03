eee78b47d5f782b03e6f8c6ea951239fe7a2ce4f
jbucsa <justin.bucsa@gmail.com>
2025-02-03 12:43:36 -0600

    Completed Anchor program using Solan and build in Rust.

--- lib.rs
+++ lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 
+// Insert Anchor Program Address
 declare_id!("");
 
 pub const ANCHOR_DISCRIMINATION_SIZE: usize = 8;
@@ -8,9 +9,15 @@ pub const ANCHOR_DISCRIMINATION_SIZE: usize = 8;
 pub mod favorites{
     use super ::*;
 
-    pub fn set_favorites()-> Return <()>{
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String, hobbies: Vec<Strings>)-> Return <()>{
         msg!("Greeting Macro!!!", context.program.id);
-    }
+        let user_public_key = context.accounts.user.key();
+
+        mes!("User {user_public_key}'s favorite number is {number}, and their favorite color is {color}. Their favorite hobbies are {hobbies:?}")
+        context.accounts.favorites.set_innner( Favorites{number, color, hobbies})
+    };
+
+    Ok(())
 }
 
 #[account]
